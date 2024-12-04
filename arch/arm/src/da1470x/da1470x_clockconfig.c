fdf945217cadf43873935ec3f5775bf8413e8b34
AlmAck <gluca86@gmail.com>
2024-12-04 23:39:32 +0100

    extend the HFCLK section
    to be fully implemented

--- arch/arm/src/da1470x/da1470x_clockconfig.c
+++ arch/arm/src/da1470x/da1470x_clockconfig.c
@@ -124,7 +124,7 @@ uint32_t regval;
 
   /* Initialize Low Power clock */
 
-#if defined(CLOCK_LPCLKSRC_SRC_RCLP)
+#if defined(CLOCK_LPCLK_SRC_RCLP)
   /* Enable RCLP clock */
 
   regval = getreg32(DA1470_CRG_TOP_CLK_RCLP);
@@ -137,7 +137,7 @@ uint32_t regval;
   regval = (regval & ~CRG_TOP_LP_CLK_SEL_MASK) | (0x00 << CRG_TOP_LP_CLK_SEL_POS);
   putreg32(regval, DA1470_CRG_TOP_CLK_CTRL);
 
-#elif defined(CLOCK_LPCLKSRC_SRC_RCX)
+#elif defined(CLOCK_LPCLK_SRC_RCX)
   /* Enable RCX clock */
 
   regval = getreg32(DA1470_CRG_TOP_CLK_RCX);
@@ -150,7 +150,7 @@ uint32_t regval;
   regval = (regval & ~CRG_TOP_LP_CLK_SEL_MASK) | (0x01 << CRG_TOP_LP_CLK_SEL_POS);
   putreg32(regval, DA1470_CRG_TOP_CLK_CTRL);
 
-#elif defined(CLOCK_LPCLKSRC_SRC_XTAL32K)
+#elif defined(CLOCK_LPCLK_SRC_XTAL32K)
   /* Enable XTAL32K clock */
 
   regval = getreg32(DA1470_CRG_TOP_CLK_XTAL32K);
@@ -163,7 +163,7 @@ uint32_t regval;
   regval = (regval & ~CRG_TOP_LP_CLK_SEL_MASK) | (0x02 << CRG_TOP_LP_CLK_SEL_POS);
   putreg32(regval, DA1470_CRG_TOP_CLK_CTRL);
 
-#elif defined(CLOCK_LPCLKSRC_SRC_EXTERNAL)
+#elif defined(CLOCK_LPCLK_SRC_EXTERNAL)
   /* No action needed for external clock */
 
   /* External clock uses the default setup or is set elsewhere */
@@ -172,8 +172,8 @@ uint32_t regval;
 
 /* Initialize system clock */
 
-#if defined(CLOCK_SYSCLKSRC_SRC_RCHS_32)
-#elif defined(CLOCK_SYSCLKSRC_SRC_XTAL32M)
+#if defined(CLOCK_HFCLK_SRC_RCHS_32)
+#elif defined(CLOCK_XTAL32M_SRC)
   xtal32m_settled = (REG_GETF(CRG_XTAL, XTAL32M_STAT0_REG, XTAL32M_READY));
   if (xtal32m_settled == false) {
     // XTAL32M has not been started. Use PDC to start it.
@@ -186,14 +186,14 @@ uint32_t regval;
     GLOBAL_INT_RESTORE();
   }
 
-#elif defined(CLOCK_SYSCLKSRC_SRC_RCHS_64)
-#elif defined(CLOCK_SYSCLKSRC_SRC_RCHS_96)
-#elif defined(CLOCK_SYSCLKSRC_SRC_PLL160)
+#elif defined(CLOCK_HFCLK_SRC_RCHS_64)
+#elif defined(CLOCK_HFCLK_SRC_RCHS_96)
+#elif defined(CLOCK_SYSCLKSRC_SRC_PLL160) //TODO
 #endif
 
-#if defined(CONFIG_NRF53_LFCLK_RC)
-  /* TODO: calibrate LFCLK RC oscillator */
-#endif
+// #if defined(CONFIG_NRF53_LFCLK_RC)
+//   /* TODO: calibrate LFCLK RC oscillator */
+// #endif
 #endif
 }
 
